c889d1a9b50407659e24390dd0cc54efa0dfa55e
haris18896 <haris18896@gmail.com>
2025-04-17 13:07:35 +0500

    validation has been added

--- identity-service/src/utils/validation.js
+++ identity-service/src/utils/validation.js
@@ -1,9 +1,10 @@
 const Joi = require("joi");
 
 const validationRegistration = (data) => {
+  console.log("check here...");
   const schema = Joi.object({
-    username: Joi.string().required(),
-    password: Joi.string().required(),
+    username: Joi.string().min(3).max(50).required(),
+    password: Joi.string().min(6).max(1024).required(),
     email: Joi.string().email().required(),
   });
   return schema.validate(data);
